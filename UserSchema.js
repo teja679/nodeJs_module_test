d02f6b4df6fc3b9b2dc613c73f4ca63b5c9019a9
teja679 <lovlytejaa@gmail.com>
2022-12-31 09:39:12 +0530

    registartion form redirect to login page

---
+++ UserSchema.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: false,
+  },
+});
+
+module.exports = mongoose.model("users", userSchema);
